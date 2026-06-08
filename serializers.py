8a74f6374df260aa0caffe9a876852d7e0a02f74
dmaxtop <almumit123465@gmail.com>
2026-06-08 11:08:00 +0600

    feat: add serializers for Account model with list and detail views

--- serializers.py
+++ serializers.py
@@ -1,8 +1,22 @@
 # serializers.py
 from rest_framework import serializers
 from models import Transaction
+from models import Account
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
+
+
+
+class AccountListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'account_number', 'account_type', 'is_frozen']
+
+class AccountDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'account_number', 'account_type', 'balance', 'is_frozen', 'user']
+        read_only_fields = ['user', 'balance']
