25a974a95d046790b0edc6e45d9fe34a5695f0b3
dmaxtop <almumit123465@gmail.com>
2026-06-08 13:04:17 +0600

    feat: move API routing to a dedicated urls.py file for better organization

--- bank_project/urls.py
+++ bank_project/urls.py
@@ -1,12 +1,8 @@
-# api/urls.py
+# bank_project/urls.py
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AccountViewSet, TransactionViewSet
-
-router = DefaultRouter()
-router.register(r'accounts', AccountViewSet, basename='account')
-router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Seamless application routing isolation
 ]
