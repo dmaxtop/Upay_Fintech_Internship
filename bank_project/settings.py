dafc6eff20ed2bb20c1d7059a1145dc97be75288
dmaxtop <almumit123465@gmail.com>
2026-06-08 13:40:09 +0600

    feat: add INSTALLED_APPS configuration in settings.py for Django project

---
+++ bank_project/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third-Party Vendor Frameworks
+    'rest_framework',
+    
+    # Local Custom Modular Applications
+    'api.apps.ApiConfig', 
+]
