3a6ee8da74228b17142c9d33ac41b067cfec6a1e
dmaxtop <almumit123465@gmail.com>
2026-06-08 13:01:31 +0600

    refactor: reorganize serializers to support django app conventions

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,21 @@
-# serializers.py
+# api/serializers.py
 from rest_framework import serializers
-from api.models import Transaction
-from api.models import Account
-
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = '__all__'
-
-
+from .models import Account, Transaction
 
 class AccountListSerializer(serializers.ModelSerializer):
+    """Task 4: Lightweight serialization context for lists (excludes balance info)"""
     class Meta:
         model = Account
         fields = ['id', 'account_number', 'account_type', 'is_frozen']
 
 class AccountDetailSerializer(serializers.ModelSerializer):
+    """Task 4: Complete high-fidelity fields representation for details views"""
     class Meta:
         model = Account
         fields = ['id', 'account_number', 'account_type', 'balance', 'is_frozen', 'user']
         read_only_fields = ['user', 'balance']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
