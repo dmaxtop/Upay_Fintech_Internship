08a57d98eebda2ed0641d6c5dfff5ef54069c892
dmaxtop <almumit123465@gmail.com>
2026-06-15 18:11:10 +0600

    fix: fixing typo for model.py

--- ledger/models.py
+++ ledger/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, realted_name= 'account')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name= 'account')
     account_number = models.CharField(max_length=32, unique=True)
     balance_usd = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
